cd20d9a4bdddaa7bda2aacc2933a9c54f1359ebf
Dórian Langbeck <langbeck@hpe.com>
2021-02-17 20:05:22 -0300

    Add prepare-master.sh

---
+++ prepare-master.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e -o pipefail
+cd $(dirname $(realpath "${0}"))
+
+echo >&2 "Provisioning spire-agent credentials..."
+./gencred-agent.sh $(hostname) | base64 -d | tar -xzC /
+
+echo >&2 "Copying spire manifests..."
+rm -f /etc/kubernetes/manifests/spire-*.yaml
+cp kubernetes/manifests/spire-server.yaml /etc/kubernetes/manifests/
+cp kubernetes/manifests/spire-agent.yaml /etc/kubernetes/manifests/
