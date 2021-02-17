06b211ab37ba58292fe304f5085ffda7d91016c7
Dórian Langbeck <langbeck@hpe.com>
2021-02-17 20:08:49 -0300

    Fix local and provisioned bundle paths

--- gencred-agent.sh
+++ gencred-agent.sh
@@ -16,7 +16,7 @@ trap 'rm -rf ${output_dir}' TERM INT EXIT
 ## RootFS
 rootfs_dir="${output_dir}/rootfs"
 kubernetes_dir="${rootfs_dir}/etc/kubernetes"
-spire_conf_dir="${rootfs_dir}/bundle/spire/conf"
+spire_conf_dir="${rootfs_dir}/node-bundle/spire/conf"
 
 ## Prepare essential folders and resources
 mkdir -p "${spire_conf_dir}"
@@ -27,17 +27,17 @@ cp "kubernetes/manifests/spire-agent.yaml" "${kubernetes_dir}/manifests/"
 cp "/etc/kubernetes/pki/ca.crt" "${kubernetes_dir}/pki/ca.crt"
 
 ## Generate keys
-openssl req -newkey rsa:2048 -nodes                         \
-    -out    "${output_dir}/x509pop_user.csr"                \
-    -keyout "${spire_conf_dir}/agent/x509pop_user.key"      \
+openssl req -newkey rsa:2048 -nodes                     \
+    -out    "${output_dir}/x509pop_user.csr"            \
+    -keyout "${spire_conf_dir}/agent/x509pop_user.key"  \
     -subj   "/CN=${node_name}"
 
-openssl x509 -req -CAcreateserial                           \
-    -in    "${output_dir}/x509pop_user.csr"                 \
-    -out   "${spire_conf_dir}/agent/x509pop_user.crt"       \
-    -CA    "/node-bundle/spire/conf/server/x509pop_ca.crt"  \
-    -CAkey "/node-bundle/spire/conf/server/x509pop_ca.key"  \
-    -extfile <(echo keyUsage=digitalSignature)              \
+openssl x509 -req -CAcreateserial                       \
+    -in    "${output_dir}/x509pop_user.csr"             \
+    -out   "${spire_conf_dir}/agent/x509pop_user.crt"   \
+    -CA    "/bundle/spire/conf/server/x509pop_ca.crt"   \
+    -CAkey "/bundle/spire/conf/server/x509pop_ca.key"   \
+    -extfile <(echo keyUsage=digitalSignature)          \
     -days 3650
 
 ## Create provisioning bundle
