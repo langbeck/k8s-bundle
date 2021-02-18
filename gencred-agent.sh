dc4504557de0b622f7fb88fcef0dbc78264bea09
Dórian Langbeck <langbeck@hpe.com>
2021-02-17 23:47:40 -0300

    Add kubelet.conf to gencred-agent.sh

--- gencred-agent.sh
+++ gencred-agent.sh
@@ -9,9 +9,10 @@ fi
 
 node_name="${1}"
 
+SERVER_URL=$(kubectl config view -o jsonpath='{.clusters[0].cluster.server}')
+
 ## Resolve and export server address
-SERVER_ADDRESS=$(kubectl config view -o jsonpath='{.clusters[0].cluster.server}')
-SERVER_ADDRESS=${SERVER_ADDRESS#https://}
+SERVER_ADDRESS=${SERVER_URL#https://}
 SERVER_ADDRESS=${SERVER_ADDRESS//:*/}
 export SERVER_ADDRESS
 
@@ -22,7 +23,8 @@ trap 'rm -rf ${output_dir}' TERM INT EXIT
 ## RootFS
 rootfs_dir="${output_dir}/rootfs"
 kubernetes_dir="${rootfs_dir}/etc/kubernetes"
-spire_conf_dir="${rootfs_dir}/node-bundle/spire/conf"
+node_bundle_dir="${rootfs_dir}/node-bundle"
+spire_conf_dir="${node_bundle_dir}/spire/conf"
 
 ## Prepare essential folders and resources
 mkdir -p "${spire_conf_dir}"
@@ -32,6 +34,38 @@ envsubst < "spire/conf/agent/agent.conf" > "${spire_conf_dir}/agent/agent.conf"
 mkdir -p "${kubernetes_dir}/manifests" "${kubernetes_dir}/pki"
 cp "kubernetes/manifests/spire-agent.yaml" "${kubernetes_dir}/manifests/"
 cp "/etc/kubernetes/pki/ca.crt" "${kubernetes_dir}/pki/ca.crt"
+
+## SPIRE Agent binary and authn-plugin
+mkdir -p "${rootfs_dir}/opt/spire/bin"
+cp "/opt/spire/bin/spire-agent" "${rootfs_dir}/opt/spire/bin/spire-agent"
+cp "/bundle/spire-authn-plugin.sh" "${node_bundle_dir}/spire-authn-plugin.sh"
+
+## Generate kubelet config
+CA_DATA=$(base64 -w0 "/bundle/spire/conf/server/dummy_upstream_ca.crt")
+cat <<EOF > ${rootfs_path}/etc/kubernetes/kubelet.conf
+apiVersion: v1
+kind: Config
+
+clusters:
+- name: kubernetes
+  cluster:
+    certificate-authority-data: ${CA_DATA}
+    server: ${SERVER_URL}
+
+current-context: system:node:${node_name}@kubernetes
+contexts:
+- name: system:node:${node_name}@kubernetes
+  context:
+    cluster: kubernetes
+    user: system:node:${node_name}
+
+users:
+- name: system:node:${node_name}
+  user:
+    exec:
+      apiVersion: client.authentication.k8s.io/v1beta1
+      command: /node-bundle/spire-authn-plugin.sh
+EOF
 
 ## Generate keys
 openssl req -newkey rsa:2048 -nodes                     \
