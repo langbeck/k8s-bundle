c60943771775392b4fb9a322e98d6aff1fac5561
Dórian Langbeck <langbeck@hpe.com>
2021-02-17 22:48:34 -0300

    Create symlink during intall-deps.sh

--- install-deps.sh
+++ install-deps.sh
@@ -6,3 +6,6 @@ yum install -y openssl gettext
 mkdir -p /opt/spire
 curl -L https://github.com/spiffe/spire/releases/download/v0.10.0/spire-0.10.0-linux-x86_64-glibc.tar.gz |\
     tar --strip-components=2 -xzC /opt/spire
+
+ln -s /opt/spire/bin/spire-server /usr/local/bin/spire-server
+ln -s /opt/spire/bin/spire-agent /usr/local/bin/spire-agent
