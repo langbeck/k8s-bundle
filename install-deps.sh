36a69ad960111301241a7c18aae76321adc41a8f
Dórian Langbeck <langbeck@hpe.com>
2021-02-18 00:19:36 -0300

    Add net-tools to install-deps.sh

--- install-deps.sh
+++ install-deps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e -o pipefail
 
-yum install -y openssl gettext
+yum install -y openssl gettext net-tools
 
 mkdir -p /opt/spire
 curl -L https://github.com/spiffe/spire/releases/download/v0.10.0/spire-0.10.0-linux-x86_64-glibc.tar.gz |\
